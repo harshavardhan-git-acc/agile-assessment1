b828926764214c0ee37cc8e4d302d9a906c21004
unknown <Administrator@SJT219SITE023.VITUNIVERSITY.LOCAL>
2026-03-26 16:38:51 +0530

    first commit

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+ public static void main(String[] args) throws Exception {
+ System.out.println("Java CI/CD Application Started...");
+ while (true) {
+ Thread.sleep(5000);
+ System.out.println("Harshavardhan D 23MIS0220 Responding...!");
+ }
+ }
+}
